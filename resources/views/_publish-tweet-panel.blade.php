a07aeb801ad5486d1c3def7d05323d55e3680b9e
Stefan Ovcharenko <ov4ar08@gmail.com>
2020-09-11 20:26:58 +0300

    Timeline page constructed

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,22 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+                    <textarea
+                        name="body"
+                        class="w-full"
+                        placeholder="What's up doc?"
+                    ></textarea>
+
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+            <img
+                src="https://eu.ui-avatars.com/api/?size=40"
+                alt=""
+                class="rounded-full"
+            >
+
+            <button type="submit" class="bg-blue-500 shadow rounded-lg py-2 px-2 text-white">Tweet-a-roo!</button>
+        </footer>
+
+    </form>
+</div>
